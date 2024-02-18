0274bf2926ed20fc7659df7d10434bd0697ccf3b
v <v@mail.ru>
2024-02-18 15:50:29 +0600

    lesson_15: начали функции высшего порядка

--- PYTHON/lesson_15/lesson_15.py
+++ PYTHON/lesson_15/lesson_15.py
@@ -1,26 +1,60 @@
 """
-Develop branch
+Lesson 15
+18.02.2024
+- функции высшего порядка
 """
+from typing import Callable, Iterable, List
+
+# map
+# filter
+# sorted
+
+# Давайте напишем функцию, которая принимает *args
+# и функцию, применяет эту функцию к каждому элементу *args, и возвращает список результатов
+
+some_list = ['хлеб', 'молоко', 'яйца', 'сахар', 'мука']
 
 
-def input_data() -> str:
-    return input('Введите строку')
+def some_func(item: str) -> str:
+    return item.upper()
 
 
-def print_data(data: str) -> None:
-    print(data)
+def apply_func_to_list(func: Callable, *args: Iterable[str]) -> List[str]:
+    result = []
+    for item in args:
+        result.append(func(item))
+
+    return result
 
 
-def exit_program() -> None:
-    input('Нажмите Enter для выхода')
+super_print = print
+super_print('Hello, world!')
+
+test_result = apply_func_to_list(some_func, *some_list)
+print(f'{test_result=}')
+
+# map - это встроенная функция, которая делает то же самое
+# map(функция, итерируемый объект) -> map object (генератор)
+# Это легко можно превратить в список
+
+result = list(map(some_func, some_list))
+result_comprihension = [some_func(item) for item in some_list]
 
 
-def main() -> None:
-    data = input_data()
-    print_data(data)
-    exit_program()
+# Filter - это встроенная функция, которая фильтрует элементы итерируемого объекта
+# filter(функция, итерируемый объект) -> filter object (генератор)
+# Функция для фильтрации должна возвращать True или False
+
+def is_letter_in_string(item: str) -> bool:
+    """
+    Функция возвращает True если в строке есть буква а
+    :param item:
+    :return:
+    """
+    return 'а' in item
 
 
-if __name__ == '__main__':
-    main()
-   
+result = list(filter(is_letter_in_string, some_list))
+result = [item for item in some_list if is_letter_in_string(item)]
+
+print(f'{result=}')
