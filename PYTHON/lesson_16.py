d720f8ae7c84e62f0e03f3ae50b5aa39bad13b18
v <v@mail.ru>
2024-03-02 13:32:04 +0500

    lesson_16: перепаковка словарей в list comprehension

--- PYTHON/lesson_16.py
+++ PYTHON/lesson_16.py
@@ -6,7 +6,9 @@ Lesson 16
 - условие в конце
 - list comprehension
 - dict comprehension
+- перепаковка данных
 
+2. Функции высшего порядка
 """
 from pprint import pprint
 
@@ -39,3 +41,18 @@ new_small_dict = {key.replace("Ч", "ЧЧ") if 'ё' in key else key: value for k
 
 # full_dict - получаем копию
 new_full_dict = {key: value for key, value in full_dict.items()}
+# pprint(new_full_dict, sort_dicts=False)
+
+# Список словарей full_dict
+full_list = [value for value in full_dict.values()]
+# pprint(full_list, sort_dicts=False)
+
+# Список словарей full_dict, добавляем внешний ключ как значение в каждый словарь в списке словарей
+# Получаем список словарей с ключом 'id', который равен ключу словаря (key)
+full_list2 = [{'id': key, **value} for key, value in full_dict.items()]
+# pprint(full_list2, sort_dicts=False)
+
+full_lst3 = [{'айди': key, 'название': value['title'], 'год': value['year'] if value['year'] != 'TBA' else 0} for
+             key, value in full_dict.items()]
+pprint(full_lst3, sort_dicts=False)
+# Ключ id только внутри каждого словаря
