b137b1598262a3b381bbb9c773df61fd136a9ac0
v <v@mail.ru>
2024-03-02 13:00:08 +0500

    lesson_16: повторение dict comprehension

--- PYTHON/lesson_16.py
+++ PYTHON/lesson_16.py
@@ -2,28 +2,40 @@
 Lesson 16
 1. Повторение
 - comprehension
+- условие в начале
+- условие в конце
+- list comprehension
+- dict comprehension
 
 """
 from pprint import pprint
 
 from data.marvel import simple_set, small_dict, full_dict
 
-# 1. Повторение
-# - comprehension
+# 1.2. Словарные включения
 
-# 1.1. Списковые включения
-simple_list = [film for film in simple_set]
+# Простая копия small_dict
+new_small_dict = {key: value for key, value in small_dict.items()}
+# pprint(new_small_dict, sort_dicts=False)
 
-# Условие в конце - фильтрация
-simple_list1 = [film for film in simple_set if film.startswith('Ч')]
+# Условие после. Фильтрация - год не None
+new_small_dict = {key: value for key, value in small_dict.items() if value is not None}
+# pprint(new_small_dict, sort_dicts=False)
 
-# Условие в начале - альтернативное значение
-simple_list2 = [film.replace('-', '@') if '-' in film else film for film in simple_set]
+# Условие после. Фильмы где год выпуска не None и 2020 и новее
+new_small_dict = {key: value for key, value in small_dict.items() if value is not None and value >= 2020}
+# pprint(new_small_dict, sort_dicts=False)
 
-# Комбо условий
-simple_list3 = [film.replace('-', '@') if '-' in film else film for film in simple_set if film.startswith('Ч')]
+# Условие после. Фильмы где год выпуска не None и 2020 и новее
+# Замена символов в ключе
+new_small_dict = {key.replace("Ч", "ЧЧ"): value for key, value in small_dict.items() if
+                  value is not None and value >= 2020}
+# pprint(new_small_dict, sort_dicts=False)
 
-pprint(f'{simple_list=}')
-pprint(f'{simple_list1=}')
-pprint(f'{simple_list2=}')
-pprint(f'{simple_list3=}')
+# Реплейс в ключе по условию, если в строке есть буква ё
+new_small_dict = {key.replace("Ч", "ЧЧ") if 'ё' in key else key: value for key, value in small_dict.items() if
+                  value is not None and value >= 2020}
+# pprint(new_small_dict, sort_dicts=False)
+
+# full_dict - получаем копию
+new_full_dict = {key: value for key, value in full_dict.items()}
