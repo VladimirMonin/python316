b69c4b4a66559408a91a69209fbf2400bb7bf2fa
v <v@mail.ru>
2024-03-02 14:38:33 +0500

    lesson_16: метод sort

--- PYTHON/lesson_16.py
+++ PYTHON/lesson_16.py
@@ -12,69 +12,35 @@ Lesson 16
 - my_map - пишем сами map
 - map - применение функции к каждому элементу коллекции
 - filter - фильтрация коллекции
+- sort - метод сортировки коллекции
+- sorted - функция сортировки коллекции
 """
 from pprint import pprint
 from typing import Callable, Iterable, List, Set, Tuple, Any
 
 from data.marvel import simple_set, small_dict, full_dict
 
-# 2. Функции высшего порядка
-"""
-Допустим, у нас есть коллекция, и мы хотим к каждому элементу применить функцию.
-При функциональном стиле, нам нужно написать функцию, которая принимает функцию и коллекцию.
-А внутри этой функции мы применяем функцию к каждому элементу коллекции в цикле.
+simple_list = list(simple_set)
+simple_list.sort()
+print(simple_list)
 
-Давайте это напишем!
-"""
-
-
-def my_map(func: Callable, collection: List | Set | Tuple) -> List:
-    """
-    Функция высшего порядка, которая принимает функцию и коллекцию.
-    После чего применяет функцию к каждому элементу коллекции
-    :param func: функция
-    :param collection: коллекция
-    :return:
-    """
-    result = []
-    for item in collection:
-        result.append(func(item))
-    return result
+# Передаем ключ len в качестве аргумента
+simple_list.sort(key=len)
+print(simple_list)
 
 
-def my_filter(func: Callable[[Any], bool], collection: List | Set | Tuple) -> List:
-    """
-    Функция высшего порядка, которая принимает функцию и коллекцию.
-    После чего фильтрует коллекцию по функции
-    :param func: функция
-    :param collection: коллекция
-    :return:
-    """
-    result = []
-    for item in collection:
-        if func(item):
-            result.append(item)
-    return result
+def sort_by_last_letter(title: str) -> str:
+    return title[-1]
 
 
-def filter_film_by_first_letter(film: str, letter: str = 'Ч') -> bool:
-    """
-    Функция фильтрации фильмов по первой букве
-    :param letter: буква
-    :param film: название фильма
-    :return: результат фильтрации
-    """
-    return film[0].lower() == letter.lower()
+simple_list.sort(key=sort_by_last_letter)
+print(simple_list)
 
 
-# Тестируем my_filter и filter_film_by_first_letter
-# print(my_filter(filter_film_by_first_letter, simple_set))
+def sort_by_words_count(title: str) -> int:
+    return len(title.split())
 
-# Это же на комприхеншн
-# print([film for film in simple_set if filter_film_by_first_letter(film, 'ж')])
 
-# Filter - фильтрация коллекции. Встроеенная функция в Python
-# filter(функция, коллекция)
-
-# Это же на filter
-print(list(filter(filter_film_by_first_letter, simple_set)))
+# Сортировка по количеству слов в названии
+simple_list.sort(key=sort_by_words_count, reverse=True)
+print(simple_list)
