13c93eae33c9d42d2a05a7270d61b65e215d027e
v <v@mail.ru>
2024-03-03 13:58:18 +0500

    lesson_17: sorted + filter в одну строку

--- PYTHON/lesson_17.py
+++ PYTHON/lesson_17.py
@@ -3,6 +3,10 @@ Lesson 17
 03.03.2024
 - Анонимные функции
 - lambda
+- map + lambda
+- filter + lambda
+- sorted + lambda
+- сортировка по 2м признакам
 - generators?
 """
 from pprint import pprint
@@ -12,43 +16,6 @@ from PYTHON.data.marvel import simple_set, small_dict, full_dict
 
 full_list2 = [{'id': key, **value} for key, value in full_dict.items()]
 
-
-# sorted + lambda
-# sorted(iterable, key=lambda x: x['key'], reverse=True)
-
-# Сортировка по ключу title
-
-def sort_by_title(item):
-    return item['title']
-
-
-# sorted_list = sorted(full_list2, key=sort_by_title)
-# pprint(sorted_list, sort_dicts=False)
-
-# Это же + lambda
-sorted_list = sorted(full_list2, key=lambda x: x['title'][-1], reverse=True)
-# pprint(sorted_list, sort_dicts=False)
-
-# Сортировка по ключу year
-sorted_list = sorted(full_list2, key=lambda x: x['year'] if isinstance(x['year'], int) else 3000, reverse=True)
-
-
-# pprint(sorted_list, sort_dicts=False)
-
-
-# Сортировка по 2м признакам - нам надо вернуть кортеж
-
-def sort_by_year_and_title(item: dict) -> Tuple[int, str]:
-    if isinstance(item['year'], int):
-        year = item['year']
-    else:
-        year = 3000
-
-    title = item['title']
-
-    return year, title
-    # return item['year'] if isinstance(item['year'], int) else 3000, item['title']
-
-
-sorted_list = sorted(full_list2, key=sort_by_year_and_title)
-pprint(sorted_list, sort_dicts=False)
+# Фильтр (фильмы 2021 + сортировка по title в одну строку
+result = sorted(filter(lambda x: x['year'] == 2021, full_list2), key=lambda x: x['title'])
+pprint(result, sort_dicts=False)
