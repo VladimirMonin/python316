840a439ff0226a3b2c291f5761f2f77f9bcd69ee
v <v@mail.ru>
2024-03-03 14:35:22 +0500

    lesson_17: ленивое чтение CSV TXT

--- PYTHON/lesson_17.py
+++ PYTHON/lesson_17.py
@@ -7,74 +7,30 @@ Lesson 17
 - filter + lambda
 - sorted + lambda
 - сортировка по 2м признакам
-- generators?
+- generators
+- yield
+- генераторное выражение
+- ленивые вычисления
 """
+import csv
 from pprint import pprint
 from typing import Tuple
 
 
-# Генераторы - это функции, которые возвращают итераторы
-# Итератор - это объект, который возвращает свои элементы по одному за раз
-# Генераторы создаются с помощью ключевого слова yield - в переводе "давать", "уступать"
-# Генераторы могут быть бесконечными
-# Генераторы могут быть использованы в циклах for, в списковых включениях, в функции sum, max, min и т.д.
-# Генераторы могут быть переданы в функцию list, чтобы получить список из всех значений генератора
-# Генераторы могут быть переданы в функцию next, чтобы получить следующее значение генератора
-# Генераторы могут быть однострочными (генераторное выражение)
-
-# Пример генератора
-def my_generator():
-    yield 1
-    yield 2
-    yield 3
+# Ленивое чтение построчно txt файла
+def read_file(file_name: str) -> str:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for line in file:
+            yield line
 
 
-gen = my_generator()
-# print(next(gen))
-# print(next(gen))
-# print(next(gen))
-# print(next(gen))  # StopIteration
-
-for i in my_generator():
-    print(i)
+# Создание генератора "файлов"
+file_gen = read_file('data.txt')
 
 
-def my_generator_2(n):
-    for i in range(n):
-        yield i
-
-
-def my_list(n):
-    return [i for i in range(n)]
-
-
-# print(my_list(10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000))
-
-# Получаем MemoryError
-# print(my_list(100 ** 100))
-
-# for i in my_generator_2(100 ** 100):
-#     print(i)
-
-# for i in my_list(100 ** 100):
-#     print(i)
-
-# Генераторное выражение
-gen = (i for i in range(10))
-
-print(next(gen))
-gen_list = list(gen)
-
-print(gen_list)
-# print(next(gen_list)) # TypeError: 'list' object is not an iterator
-
-map_list = (map(lambda x: x ** 2, range(10)))
-print(next(map_list))
-print(next(map_list))
-print(next(map_list))
-print(next(map_list))
-print(next(map_list))
-
-for i in map(lambda x: x ** 2, range(10000000000000000000000000000000000000000000000000000)):
-    if i == 778446231616:
-        print(f'Found {i}')
+# Ленивое чтение построчно csv файла
+def read_csv(file_name: str, delimiter: str = ';') -> Tuple:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=delimiter, lineterminator='\n')
+        for row in reader:
+            yield row
