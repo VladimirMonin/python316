95e72bdba0e45d917078cc3a9e73cfd420e5a1f0
v <v@mail.ru>
2024-03-03 12:15:14 +0500

    lesson_17: подготовка

--- lesson_17.py
+++ lesson_17.py
@@ -5,3 +5,23 @@ Lesson 17
 - lambda
 - generators?
 """
+from PYTHON.data.marvel import simple_set, small_dict, full_dict
+
+# Sorted - функция высшего порядка
+# Возвращает отсортированный список
+
+simple_list = list(simple_set)
+
+sorted_list = sorted(simple_list)
+print(sorted_list)
+
+
+def sort_by_last_letter(title: str) -> str:
+    return title[-1]
+
+
+# Используем свю функцию sort_by_last_letter
+sorted_list = sorted(simple_list, key=sort_by_last_letter)
+print(sorted_list)
+
+full_list2 = [{'id': key, **value} for key, value in full_dict.items()]
