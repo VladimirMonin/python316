dd7b9ca7a0ddd531f62d2f5a92309563614da6b0
v <v@mail.ru>
2024-03-16 13:01:20 +0500

    lesson_18: замыкания

--- PYTHON/lesson_18.py
+++ PYTHON/lesson_18.py
@@ -40,15 +40,63 @@ a = 'чебурек'  # global
 
 
 def some_func():
+    # global a
     a = "шавуха"  # local
     print(f'Local: {a}')
 
     def inner_func():
+        nonlocal a  # Выйдет на уровень выше
         a = 'бургер'
         print(f'Inner: {a}')
 
     inner_func()
+    print(f'Local: {a}')
 
 
+print(f'Global: {a}')  # global
 some_func()
 print(f'Global: {a}')  # global
+
+
+# Мы можем использовать функции внутри функций в своих целях!
+
+def say_name(name: str):
+    # name - local in say_name
+    def say_goodbye():
+        print(f"Привет {name}!")
+
+    say_goodbye()
+
+
+say_name("Валера")
+
+
+def say_name2(name: str) -> Callable[[], None]:
+    # Олег тут
+    def say_goodbye():
+        print(f"Привет {name}!")
+
+    return say_goodbye
+
+
+sn: Callable = say_name2("Олег")
+# sn2: Callable = say_name2("Вася")
+# sn3: Callable = say_name2("Петя")
+sn()
+# sn2()
+# sn3()
+
+ppp = print
+ppp('Hello')
+
+"""
+Пока sn ссылается на функцию say_name2, то она не будет удалена из памяти.
+Соответственно и Олег останется в переменной name.
+
+Почему замыкание?
+
+Мы держим внутренние окружения и "замыкаем" их по цепочке
+Обратившись к sn
+
+sn -> say_name2 -> say_goodbye -> name = "Олег" 
+"""
