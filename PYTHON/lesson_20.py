36e0614a5b8b323a7553a81f1bb788ffdff164a1
v <v@mail.ru>
2024-03-23 13:04:07 +0500

    lesson_20: конструктор класса __new__ __init__

--- PYTHON/lesson_20.py
+++ PYTHON/lesson_20.py
@@ -7,21 +7,32 @@ Lesson 20
 Создание экземпляра класса
 Проверка типа объекта
 Атрибуты класса
+Магический метод __init__ (дандер метод) - double underscore
+Магический метод __new__
+Конструктор класса
+Как происходит создание экземпляра класса
 """
 
 
-# атрибуты класса
+# __init__ - конструктор класса
+# Объявление класса
+class MoscvichCar:
+    # Используем дандер метод __init__ - инициализатор класса
+    def __init__(self, model: str, year: int, color: str = 'orange'):
+        # Наделяем конкретный экземпляр класса атрибутами
+        self.model = model
+        self.year = year
+        self.color = color
+        print(f'Экземпляр класса {self.__class__.__name__} создан!')
 
-class Person:
-    name = 'Дмитрий'
+    def __new__(cls, *args, **kwargs):
+        print(f'Создание экземпляра класса {cls.__name__} c аргументами {args}')
+        return super().__new__(cls)
 
 
-Person.name = 'Александр'
-
-# Создание экземпляра класса
-person = Person()
-person2 = Person()
-person2.name = 'Алексей'
-Person.name = 'Иван'
-print(person.name)
-print(person2.name)
+m1 = MoscvichCar('412', 1980, 'red')
+print(f'Модель: {m1.model}, Год выпуска: {m1.year}, Цвет: {m1.color}')
+m2 = MoscvichCar('412', 1980, 'black')
+print(f'Модель: {m2.model}, Год выпуска: {m2.year}, Цвет: {m2.color}')
+m3 = MoscvichCar('412', 1980)
+print(f'Модель: {m3.model}, Год выпуска: {m3.year}, Цвет: {m3.color}')
