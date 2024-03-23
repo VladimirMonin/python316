54849aeaf0397f55de2eed7acdd931763f5ea58c
v <v@mail.ru>
2024-03-23 13:12:22 +0500

    lesson_20: атрибут экземпляра и инициализатор

--- PYTHON/lesson_20.py
+++ PYTHON/lesson_20.py
@@ -11,6 +11,7 @@ Lesson 20
 Магический метод __new__
 Конструктор класса
 Как происходит создание экземпляра класса
+Атрибут экземпляра
 """
 
 
@@ -23,16 +24,7 @@ class MoscvichCar:
         self.model = model
         self.year = year
         self.color = color
-        print(f'Экземпляр класса {self.__class__.__name__} создан!')
-
-    def __new__(cls, *args, **kwargs):
-        print(f'Создание экземпляра класса {cls.__name__} c аргументами {args}')
-        return super().__new__(cls)
+        print(f'Инициализация экземпляра класса {self.__class__.__name__} завершена!')
 
 
 m1 = MoscvichCar('412', 1980, 'red')
-print(f'Модель: {m1.model}, Год выпуска: {m1.year}, Цвет: {m1.color}')
-m2 = MoscvichCar('412', 1980, 'black')
-print(f'Модель: {m2.model}, Год выпуска: {m2.year}, Цвет: {m2.color}')
-m3 = MoscvichCar('412', 1980)
-print(f'Модель: {m3.model}, Год выпуска: {m3.year}, Цвет: {m3.color}')
