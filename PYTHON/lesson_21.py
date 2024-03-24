e95fd55755489e5e4efe5ffb84c2fb3d95418b00
v <v@mail.ru>
2024-03-24 13:03:46 +0500

    lesson_21: решение практики

--- PYTHON/lesson_21.py
+++ PYTHON/lesson_21.py
@@ -65,3 +65,41 @@ eval(eval_string)
 Произведите обратное преобразование с помощью функции `eval()`.
 
 """
+
+
+# Создание класса
+
+class Car:
+    def __init__(self, model, year, color, price):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.price = price
+
+    def __str__(self):
+        return f'Модель: {self.model}, год: {self.year}, цвет: {self.color}, цена: {self.price}'
+
+    def __repr__(self):
+        return f'Car("{self.model}", {self.year}, "{self.color}", {self.price})'
+
+
+# Создание объектов класса Car
+car1 = Car('BMW', 2020, 'black', 100000)
+car2 = Car('Audi', 2019, 'white', 80000)
+car3 = Car('Запорожец', 1980, 'yellow', 1000)
+
+# Создание списка объектов
+cars = [car1, car2, car3]
+
+# Вывод списка объектов
+[print(car) for car in cars]
+
+# Преобразование объектов в строки
+cars_str = [repr(car) for car in cars]
+[print(type(car)) for car in cars_str]
+print(cars_str)
+
+# Преобразование строк в объекты
+new_cars = [eval(car) for car in cars_str]
+[print(type(car)) for car in new_cars]
+print(new_cars)
