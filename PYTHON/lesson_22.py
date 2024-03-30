f26484aa809aa1d46531a76e73ec47a622944364
v <v@mail.ru>
2024-03-30 13:10:12 +0500

    lesson_22: инициализатор наследника без super

--- PYTHON/lesson_22.py
+++ PYTHON/lesson_22.py
@@ -11,75 +11,44 @@ super()
 """
 
 
-class Animal:
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-
-    def __str__(self):
-        return f'Имя: {self.name}, Возраст: {self.age}'
-
-    def voice(self):
-        print('Animal voice')
-
-
-class Dog(Animal):
-    def voice(self):
-        # Animal.voice(self)
-        super().voice()
-        print('Woof woof')
-
-
-class Cat(Animal):
-    def voice(self):
-        print('Meow meow')
-
-
-dog = Dog('Bobik', 5)
-print(dog)
-dog.voice()
-
-"""
-Практика!
-Опишите класс игрового персоонажа
-class Character
-С атрибутами name: str, health: int, mana: int, damage: int
-С методами:
-- __init__
-- __str__
-- damage - нанесение урона возвращает 1 (int)
-Опишите классы наследники
-class Warrior(Character)
-Переопределите метод damage, чтобы воин наносил урона *2
-
-class Mage(Character)
-Расширение метода.
-Вызовите метод damage родительского класса, поместите результат в переменную
-Добавьте к результату 10 и верните
-
-"""
-
-
 class Character:
-    def __init__(self, name: str, health: int, mana: int, damage: int):
+    def __init__(self, name: str, health: int, mana: int, damage: int, custom: str):
         self.name = name
         self.health = health
         self.mana = mana
         self.damage = damage
+        self.custom = custom
 
     def __str__(self):
-        return f'Имя: {self.name}, Здоровье: {self.health}, Мана: {self.mana}, Урон: {self.damage}'
+        return (f'Имя: {self.name}, Здоровье: {self.health}, Мана: {self.mana}, Урон: {self.damage}')
+
+    def get_custom(self):
+        return self.custom
 
     def damage(self):
         return self.damage()
 
 
 class Warrior(Character):
+    # Сделаем инициализатор без super
+    def __init__(self, name: str, health: int, mana: int, damage: int, damage_mod: int):
+        self.name = name
+        self.health = health
+        self.mana = mana
+        self.damage = damage
+        self.damage_mod = damage_mod
+
     def damage(self):
-        return self.damage * 2
+        return self.damage * self.damage_mod
 
 
 class Mage(Character):
     def damage(self):
         result = super().damage()
         return result + 10
+
+
+warrior = Warrior('Warrior', 100, 50, 20, 2)
+print(warrior)
+# warrior.custom = 'Warrior custom'
+warrior.get_custom()  # AttributeError: 'Warrior' object has no attribute 'custom'
