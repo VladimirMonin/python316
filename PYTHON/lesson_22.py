57407ca2dbf7891148b80071c7bd57bc81034584
v <v@mail.ru>
2024-03-30 13:23:53 +0500

    lesson_22: инициализатор наследника c super().__init__

--- PYTHON/lesson_22.py
+++ PYTHON/lesson_22.py
@@ -7,7 +7,8 @@ Lesson 22
 Переопределение методов
 Вызов метода родительского класса
 super()
-Вызов инициализатора родительского класса
+__init__ в дочернем классе без super
+зачем нужен super в __init__ наследника
 """
 
 
@@ -25,30 +26,28 @@ class Character:
     def get_custom(self):
         return self.custom
 
-    def damage(self):
-        return self.damage()
+    def get_damage(self):
+        return self.damage
 
 
 class Warrior(Character):
     # Сделаем инициализатор без super
-    def __init__(self, name: str, health: int, mana: int, damage: int, damage_mod: int):
-        self.name = name
-        self.health = health
-        self.mana = mana
-        self.damage = damage
+    def __init__(self, name: str, health: int, mana: int, damage: int, damage_mod: int, custom: str):
+        super().__init__(name, health, mana, damage, custom)
         self.damage_mod = damage_mod
 
     def damage(self):
-        return self.damage * self.damage_mod
+        final_damage = super().get_damage() * self.damage_mod
+        return final_damage
 
 
 class Mage(Character):
     def damage(self):
-        result = super().damage()
+        result = super().get_damage()
         return result + 10
 
 
-warrior = Warrior('Warrior', 100, 50, 20, 2)
+warrior = Warrior('Warrior', 100, 50, 20, 2, 'Warrior custom')
 print(warrior)
 # warrior.custom = 'Warrior custom'
 warrior.get_custom()  # AttributeError: 'Warrior' object has no attribute 'custom'
