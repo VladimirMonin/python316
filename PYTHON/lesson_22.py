d8d5fe48a3d23a35cb7270d0c61872106ec6fe70
v <v@mail.ru>
2024-03-30 12:22:35 +0500

    lesson_22: наследование без инициализаторов

---
+++ PYTHON/lesson_22.py
@@ -0,0 +1,37 @@
+"""
+Lesson 22
+30.03.2024
+
+Концепция наследования
+Наследование без init
+Переопределение методов
+Вызов метода родительского класса
+Вызов инициализатора родительского класса
+"""
+
+
+class Animal:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'Имя: {self.name}, Возраст: {self.age}'
+
+    def voice(self):
+        print('Animal voice')
+
+
+class Dog(Animal):
+    def voice(self):
+        print('Woof woof')
+
+
+class Cat(Animal):
+    def voice(self):
+        print('Meow meow')
+
+
+dog = Dog('Bobik', 5)
+print(dog)
+dog.voice()
