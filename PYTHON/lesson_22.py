c82be580354ac2ff39019009d40cf402552b5a1d
v <v@mail.ru>
2024-03-30 12:38:09 +0500

    lesson_22: практика 1

--- PYTHON/lesson_22.py
+++ PYTHON/lesson_22.py
@@ -38,3 +38,23 @@ class Cat(Animal):
 dog = Dog('Bobik', 5)
 print(dog)
 dog.voice()
+
+"""
+Практика!
+Опишите класс игрового персоонажа
+class Character
+С атрибутами name: str, health: int, mana: int, damage: int
+С методами:
+- __init__
+- __str__
+- damage - нанесение урона возвращает 1 (int)
+Опишите классы наследники
+class Warrior(Character)
+Переопределите метод damage, чтобы воин наносил урона *2
+
+class Mage(Character)
+Расширение метода.
+Вызовите метод damage родительского класса, поместите результат в переменную
+Добавьте к результату 10 и верните
+
+"""
