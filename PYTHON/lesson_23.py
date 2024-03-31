d89234352fde85114af7c57e2ad12e60173ae766
v <v@mail.ru>
2024-03-31 12:29:32 +0500

    lesson_23: наследование от общих родителей приводит к проблемам, в тч MRO

--- PYTHON/lesson_23.py
+++ PYTHON/lesson_23.py
@@ -3,6 +3,9 @@ Lesson 23
 31.03.2024
 Цепочка наследования
 Множественное наследование
+Абстрактные классы
+ABC
+@abstractmethod
 Конфликт имен
 Метод разрешения конфликтов
 Миксины
@@ -21,12 +24,27 @@ class Elf(Character):
         self.mana = mana
 
 
-class MageElf(Elf):
-    def __init__(self, name: str, health: int, mana: int, damage: int):
-        super().__init__(name, health, mana)
-        self.damage = damage
+class Dwarf(Character):
+    def __init__(self, name: str, health: int, stamina: int):
+        super().__init__(name, health)
+        self.stamina = stamina
 
 
-me = MageElf('Elf', 100, 100, 100)
-# MRO - Method Resolution Order - порядок разрешения методов
-print(MageElf.mro())
+# Этот класс вызовет проблему, потому что Elf и Dwarf имеют общего предка (Character),
+# но Python не сможет однозначно определить, в каком порядке следует вызывать методы конструктора __init__
+class ElfDwarf(Elf, Dwarf):
+    def __init__(self, name: str, health: int, mana: int, stamina: int):
+        super().__init__(name, health, mana, stamina)
+
+
+ed = ElfDwarf('Эльф', 100, 100, 100)
+
+"""
+Чтобы вызвать конфликт в порядке разрешения методов (MRO) в Python, 
+можно использовать множественное наследование с диамантовой проблемой. 
+
+Диамантовая проблема возникает, когда два класса наследуют от одного 
+базового класса, а затем еще один класс наследует от обоих этих классов. 
+
+В Python это может привести к неоднозначности порядка вызова методов.
+"""
