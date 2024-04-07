4627fcfe7a44b2249c87cbf6aba0e55c57abd168
v <v@mail.ru>
2024-04-07 14:42:38 +0500

    lesson_25: решение практики

--- PYTHON/lesson_25.py
+++ PYTHON/lesson_25.py
@@ -33,3 +33,40 @@ city: str
 через if в новый список только тех, кто старше 18 лет
 ---
 """
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    age: int
+    growth: int
+    city: str
+
+    def __post_init__(self):
+        assert self.validate_str_length(self.first_name, 3), 'Имя должно быть строкой длинной не менее 3 символов'
+        assert self.validate_str_length(self.last_name), 'Фамилия должна быть строкой длинной не менее 3 символов'
+        assert self.validate_integer(self.age, 0, 150), 'Возраст должен быть числом от 0 до 150'
+        assert self.validate_str_length(self.city, 3), 'Город должен быть строкой длинной не менее 3 символов'
+        assert self.validate_integer(self.growth, 50, 250), 'Рост должен быть числом от 50 до 250'
+
+    def __bool__(self):
+        return self.age > 18
+    
+    def validate_str_length(self, value, length=2):
+        return isinstance(value, str) and len(value) >= length
+    
+    def validate_integer(self, value, min, max):
+        return isinstance(value, int) and min <= value <= max
+    
+
+
+
+
+persons = [
+    Person('Иван', 'Степененко', 25, 180, 'Москва'),
+    Person('Лариса', 'Гузеева', 30, 200, 'Москва'),
+]
+
+adults = [person for person in persons if person]
+
+# print(len(persons[0])) # TypeError: object of type 'Person' has no len()
