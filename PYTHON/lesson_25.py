8c9359e44e09e44c662e66c91e742c010b2b6349
v <v@mail.ru>
2024-04-07 12:31:48 +0500

    lesson_25: dataclass vs обычный класс

--- PYTHON/lesson_25.py
+++ PYTHON/lesson_25.py
@@ -4,14 +4,31 @@ Lesson 25
 Dataclasses
 """
 from dataclasses import dataclass
-from tkinter import N
 
-
+"""
+title
+price
+in_stock
+url
+rating
+"""
 @dataclass
-class Person:
-    name: str
-    age: int
-    city: str = "Moscow"
+class Book:
+    title: str
+    price: float
+    in_stock: bool
+    cover_url: str
+    rating: int
 
-p = Person("Ivan", 30)
-print(p)
+
+# Это же на обычном классе
+class Book:
+    def __init__(self, title: str, price: float, in_stock: bool, cover_url: str, rating: int):
+        self.title = title
+        self.price = price
+        self.in_stock = in_stock
+        self.cover_url = cover_url
+        self.rating = rating
+
+    def __str__(self):
+        return f"Book({self.title}, {self.price}, {self.in_stock}, {self.cover_url}, {self.rating})"
